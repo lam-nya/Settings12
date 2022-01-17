e367919855b2e70109cb5275943ea9d85365a309
Igor Kuzmin <lamnya@gmail.com>
2022-01-17 19:19:00 +0800

    Added NavigationController.

--- Settings12/SceneDelegate.swift
+++ Settings12/SceneDelegate.swift
@@ -13,10 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let settingsViewController = SettingsViewController()
+        let settingsNavigationController = UINavigationController(rootViewController: settingsViewController)
+        settingsNavigationController.viewControllers = [settingsViewController]
+        settingsNavigationController.navigationBar.prefersLargeTitles = true
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = settingsNavigationController
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Settings12/SettingsViewController.swift
+++ Settings12/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingsViewController.swift
 //  Settings12
 //
 //  Created by Igor Kuzmin on 15.01.2022.
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        title = "Settings"
+        view.backgroundColor = .blue
+
     }
 
 
